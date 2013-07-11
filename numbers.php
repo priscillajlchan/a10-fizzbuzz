894c1fd90d5b169b47068a372afb67579fe53122
Priscilla <priscillachan__@hotmail.com>
2013-07-10 19:29:52 -0700

    first commit
    added php file

---
+++ numbers.php
@@ -0,0 +1,30 @@
+
+<?php
+
+for ($n = 1; $n <= 10; $n++) {
+
+	if ($n % 2 == 0 && $n % 3 == 0) {
+	echo "The number is " . $n . " fizz buzz.<br>" ;
+
+	}
+
+	else if ($n % 2 == 0) {
+		echo "The number is " . $n . " fizz.<br>" ;
+
+	}
+
+	else if ($n % 3 == 0) {
+		echo "The number is " . $n . " buzz.<br>" ;
+
+	}
+
+
+	else {
+			echo "The number is " . $n . ".<br>";
+
+	}
+
+}
+
+
+?>
